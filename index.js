96571b7b857710704c1dd7ff974e6e87dfd22a36
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-15 12:08:49 -0500

    Added images to page

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const airplaneMenuDivElement = document.getElementById('airplane-menu')
+
+
+function addAirplaneImageToMenu(flight){
+    const imgElement = document.createElement('img')
+    imgElement.src = flight.image
+    airplaneMenuDivElement.appendChild(imgElement)
+}
+
+
 fetch('http://localhost:3000/flights')
 .then(response => response.json)
 .then(flights => {
-    console.log(flights)
+    flights.forEach(addAirplaneImageToMenu)
 })
