134a1977c0c35363393729368d121ea7b56757ca
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-15 12:25:04 -0500

    Created displayFlightDetails fucntion to display flight details

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ function addAirplaneImageToMenu(flight){
     airplaneMenuDivElement.appendChild(imgElement)
 }
 
+function displayFlightDetails(flight){
+    const flightIdElement = document.getElementById("flight-id")
+    flightIdElement.textContent = `Flight # ${flight.id}`
+}
 
 fetch('http://localhost:3000/flights')
 .then(response => response.json)
 .then(flights => {
     flights.forEach(addAirplaneImageToMenu)
+
+    displayFlightDetails(flights[0])
 })
